850ba42e7aae96be4b5884d665b922b1baaa620a
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2026-04-05 21:44:09 +1000

    Updated to match new format

--- scraper.rb
+++ scraper.rb
@@ -1,31 +1,81 @@
+# frozen_string_literal: true
+
 require 'scraperwiki'
 require 'mechanize'
 require 'json'
+require 'date'
 
-agent = Mechanize.new
-# agent.log = Logger.new(STDOUT)
-url = 'https://eplanning.stonnington.vic.gov.au/EPlanning/Services/ReferenceService.svc/Get_PlanningRegister'
-req_body = File.read('req')
-headers = {"Content-Type" => "application/json"}
- 
-viewurlbase = "https://eplanning.stonnington.vic.gov.au/EPlanning/Public/ViewActivity.aspx?refid="
- 
-response = agent.request_with_entity('POST', url, req_body, headers)
-data = JSON.parse(response.body)
-d = JSON.parse(data['d'])
-av = d['ActivityView']
+class Scraper
+  BASE_URL = 'https://eplanning.stonnington.vic.gov.au'
+  API_URL = "#{BASE_URL}/planning-api/api/PublicRegister/GetRecords"
+  PAGE_SIZE = 100
+  MAX_PAGES = 100
 
-av.each do |r|
-  record = {
-    'council_reference' => r['ApplicationReference'],
-    'address' => r['SiteAddress'],
-    'description' => r['ReasonForPermit'],
-    'info_url' => (viewurlbase + URI.encode(r['ApplicationReference'])).to_s,
-    'comment_url' => "mailto:council@stonnington.vic.gov.au",
-    'date_scraped' => Date.today.to_s,
-    'date_received' => Date.strptime(r['LodgedDate_STRING'], "%d-%b-%Y").to_s
-  }
+  def self.run
+    agent = Mechanize.new
+    agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-  ScraperWiki.save_sqlite(['council_reference'], record)
+    date_from = Date.today - 30
+    page_number = 0
+    total_saved = 0
+    last_date_received = Date.today
 
+    while page_number < MAX_PAGES && last_date_received >= date_from
+      body = {
+        applicationNumber: '',
+        dateLodged: '',
+        decisionDate: nil,
+        addressDetail: '',
+        statuses: '',
+        ward: '',
+        reasonForPermit: '',
+        pageNumber: page_number,
+        pageSize: PAGE_SIZE,
+        sortBy: 'dateLodged',
+        sortDirection: 'desc',
+        parentSearch: nil,
+        dateFormat: 'd-MMM-yyyy'
+      }.to_json
+      page_number += 1
+
+      response = agent.request_with_entity('POST', API_URL, body, 'Content-Type' => 'application/json')
+      data = JSON.parse(response.body)
+      records = data['registers']
+      if records.nil? || records.empty?
+        puts "WARNING: Aborted - API did not return list of records in registers key"
+        break
+      end
+
+      records.each do |r|
+        date_lodged = r['dateLodged']&.slice(0, 10)
+        unless date_lodged
+          puts "NOTE: Skipped record missing dateLodged: #{r.inspect}"
+          next
+        end
+
+        last_date_received = Date.parse(date_lodged)
+        record = {
+          'council_reference' => r['applicationNumberDisplay'],
+          'address' => r['addressDetail'],
+          'description' => r['reasonForPermit'],
+          'info_url' => "#{BASE_URL}/public/details/#{r['correlationId']}",
+          'date_scraped' => Date.today.to_s,
+          'date_received' => last_date_received
+        }
+        puts "Saving record #{record['council_reference']} - #{record['address']}#{ENV['DEBUG'] ? " #{record['info_url']}" : ''}"
+        ScraperWiki.save_sqlite(['council_reference'], record)
+        total_saved += 1
+      end
+      continuing = last_date_received >= date_from ? "continuing on next page" : "far enough into past"
+      puts "Page #{page_number}: #{records.size} records received from #{last_date_received} (#{continuing})"
+      if records.size < PAGE_SIZE
+        puts "End of list - page is not full"
+        break
+      end
+    end
+
+    puts "Done. Total saved: #{total_saved}"
+  end
 end
+
+Scraper.run if __FILE__ == $PROGRAM_NAME
